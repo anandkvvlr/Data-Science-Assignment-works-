4cc7856104890ccc45ef7c6afd437ca0db4144b0
Anand kv <88075268+anandkvvlr@users.noreply.github.com>
2022-01-09 19:01:44 +0530

    Add files via upload

---
+++ Ensamble_algorithm/diabet.py
@@ -0,0 +1,309 @@
+#############################Bagging###########################
+import pandas as pd
+
+df = pd.read_csv("C://Users//user//Downloads//ensamble//Diabeted_Ensemble.csv")
+
+# Dummy variables
+df.head()
+df.info()
+
+# Input and Output Split
+predictors = df.loc[:, df.columns!=" Class variable"]
+type(predictors)
+
+target = df[" Class variable"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=42)
+
+
+from sklearn import tree
+clftree = tree.DecisionTreeClassifier(max_depth=7)
+from sklearn.ensemble import BaggingClassifier
+
+bag_clf = BaggingClassifier(base_estimator = clftree, n_estimators = 500,
+                            bootstrap = True, n_jobs = 1, random_state = 42)
+
+bag_clf.fit(x_train, y_train,)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Evaluation on Testing Data
+print(classification_report(y_test, bag_clf.predict(x_test)))
+print(classification_report(y_test, bag_clf.predict(x_test)))
+accuracy_score(y_test, bag_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, bag_clf.predict(x_train))
+accuracy_score(y_train, bag_clf.predict(x_train))
+
+###########################Boost#############################
+# applied adaboost
+import pandas as pd
+
+df = pd.read_csv("C://Users//user//Downloads//ensamble//Diabeted_Ensemble.csv")
+
+# Dummy variables
+df.head()
+df.info()
+
+# Input and Output Split
+predictors = df.loc[:, df.columns!=" Class variable"]
+type(predictors)
+
+target = df[" Class variable"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+
+
+# Refer to the links
+# https://scikit-learn.org/stable/modules/classes.html#module-sklearn.ensemble
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html#sklearn.ensemble.AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+ada_clf = AdaBoostClassifier(learning_rate = 0.5, n_estimators = 500)
+
+ada_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, ada_clf.predict(x_test))
+accuracy_score(y_test, ada_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, ada_clf.predict(x_train))
+accuracy_score(y_train, ada_clf.predict(x_train))
+
+
+
+############################# voting ##################################
+# Import the required libraries
+from sklearn import datasets, linear_model, svm, neighbors, naive_bayes
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Input and Output Split
+predictors = df.loc[:, df.columns!=" Class variable"]
+type(predictors)
+
+target = df[" Class variable"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+
+# Instantiate the learners (classifiers)
+learner_1 = neighbors.KNeighborsClassifier(n_neighbors=5)
+learner_2 = linear_model.Perceptron(tol=1e-2, random_state=0)
+learner_3 = svm.SVC(gamma=0.001)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_1),
+                           ('Prc', learner_2),
+                           ('SVM', learner_3)])
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+
+# Predict the most voted class
+hard_predictions_test = voting.predict(x_test)
+hard_predictions_train = voting.predict(x_train)
+
+# Accuracy of hard voting
+print('Hard Voting:\n', confusion_matrix(y_test, hard_predictions_test))
+print('Hard Voting:\n', accuracy_score(y_test, hard_predictions_test))
+print('Hard Voting:\n', confusion_matrix(y_train, hard_predictions_train))
+print('Hard Voting:\n', accuracy_score(y_train, hard_predictions_train))
+
+
+#################
+
+# Soft Voting # 
+# Instantiate the learners (classifiers)
+learner_4 = neighbors.KNeighborsClassifier(n_neighbors = 5)
+learner_5 = naive_bayes.GaussianNB()
+learner_6 = svm.SVC(gamma = 0.001, probability = True)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_4),
+                           ('NB', learner_5),
+                           ('SVM', learner_6)],
+                            voting = 'soft')
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+learner_4.fit(x_train, y_train)
+learner_5.fit(x_train, y_train)
+learner_6.fit(x_train, y_train)
+
+# Predict the most probable class
+soft_predictions_test = voting.predict(x_test)
+soft_predictions_train = voting.predict(x_train)
+
+# Get the base learner predictions
+predictions_4 = learner_4.predict(x_test)
+predictions_5 = learner_5.predict(x_test)
+predictions_6 = learner_6.predict(x_test)
+
+# Accuracies of base learners
+print('L4:', accuracy_score(y_test, predictions_4))
+print('L5:', accuracy_score(y_test, predictions_5))
+print('L6:', accuracy_score(y_test, predictions_6))
+
+# Accuracy of Soft voting
+print('soft Voting:\n', confusion_matrix(y_test, soft_predictions_test))
+print('soft Voting:\n', accuracy_score(y_test, soft_predictions_test))
+print('soft Voting:\n', confusion_matrix(y_train, soft_predictions_train))
+print('soft Voting:\n', accuracy_score(y_train, soft_predictions_train))
+
+
+#################################stacking################################
+# Libraries and data loading
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+#from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df1=df.copy(deep=True)
+
+# converting ouput variable to numeric binary dummy variable format
+lb = LabelEncoder()
+df1[" Class variable"] =lb.fit_transform(df1[" Class variable"])
+# Input and Output Split
+predictors = df1.loc[:, df1.columns!=" Class variable"]
+type(predictors)
+
+target = df1[" Class variable"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+train_x, test_x, train_y, test_y = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+
+#converting to nu numpy array
+train_x = train_x.values
+test_x = test_x.values
+train_y = train_y.values
+test_y = test_y.values
+
+# Create the ensemble's base learners and meta learner
+# Append base learners to a list
+base_learners = []
+
+# KNN classifier model
+knn = KNeighborsClassifier(n_neighbors=2)
+base_learners.append(knn)
+
+# Decision Tree Classifier model
+dtr = DecisionTreeClassifier(max_depth=4, random_state=123456)
+base_learners.append(dtr)
+
+# Multi Layered Perceptron classifier
+mlpc = MLPClassifier(hidden_layer_sizes =(100, ), solver='lbfgs', random_state=123456)
+base_learners.append(mlpc)
+
+# Meta model using Logistic Regression
+meta_learner = LogisticRegression(solver='lbfgs')
+
+# Create the training meta data
+
+# Create variables to store meta data and the targets
+meta_data = np.zeros((len(base_learners), len(train_x )))
+meta_targets = np.zeros(len(train_x))
+
+# Create the cross-validation folds
+KF = KFold(n_splits = 5)
+meta_index = 0
+for train_indices, test_indices in KF.split(train_x):
+    # Train each learner on the K-1 folds and create meta data for the Kth fold
+    for i in range(len(base_learners)):
+        learner = base_learners[i]
+
+        learner.fit(train_x[train_indices], train_y[train_indices])
+        predictions = learner.predict_proba(train_x[test_indices])[:,0]
+
+        meta_data[i][meta_index:meta_index+len(test_indices)] = predictions
+
+    meta_targets[meta_index:meta_index+len(test_indices)] = train_y[test_indices]
+    meta_index += len(test_indices)
+
+# Transpose the meta data to be fed into the meta learner
+meta_data = meta_data.transpose()
+
+# Create the meta data for the test set and evaluate the base learners
+test_meta_data = np.zeros((len(base_learners), len(test_x)))
+base_acc = []
+
+for i in range(len(base_learners)):
+    learner = base_learners[i]
+    learner.fit(train_x, train_y)
+    predictions = learner.predict_proba(test_x)[:,0]
+    test_meta_data[i] = predictions
+
+    acc = metrics.accuracy_score(test_y, learner.predict(test_x))
+    base_acc.append(acc)
+test_meta_data = test_meta_data.transpose()
+
+# Fit the meta learner on the train set and evaluate it on the test set
+meta_learner.fit(meta_data, meta_targets)
+ensemble_predictions = meta_learner.predict(test_meta_data)
+
+acc = metrics.accuracy_score(test_y, ensemble_predictions)
+ 
+# Print the results
+for i in range(len(base_learners)):
+    learner = base_learners[i]
+
+    print(f'{base_acc[i]:.2f} {learner.__class__.__name__}')
+    
+print(f'{acc:.2f} Ensemble')
+
+print(classification_report(test_y, ensemble_predictions))
+print('ensamble:\n', accuracy_score(test_y, ensemble_predictions))
+print('ensamble:\n', confusion_matrix(test_y, ensemble_predictions))
+
+
+####### GridSearchCV  #####
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+rf_clf_grid = RandomForestClassifier(n_estimators=500, n_jobs=1, random_state=42,bootstrap=True)
+
+param_grid = {"max_features": [4, 5, 6, 7, 8], "min_samples_split": [2, 3, 4, 5,6,7,8],"max_leaf_nodes":[4,5,6]}
+
+grid_search = GridSearchCV(rf_clf_grid, param_grid, n_jobs = -1, cv = 5, scoring = 'accuracy')
+
+grid_search.fit(x_train, y_train)
+
+grid_search.best_params_
+
+cv_rf_clf_grid = grid_search.best_estimator_
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+confusion_matrix(y_test, cv_rf_clf_grid.predict(x_test))
+accuracy_score(y_test, cv_rf_clf_grid.predict(x_test))
+
+confusion_matrix(y_train, cv_rf_clf_grid.predict(x_train))
+accuracy_score(y_train, cv_rf_clf_grid.predict(x_train))
+
+
+
+
